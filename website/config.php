224c6c69467e93d3ee6796d01d119506a5c57b09
Nephiaust <nephi.aust@gmail.com>
2022-08-16 16:40:36 +0930

    Create config.php

---
+++ website/config.php
@@ -0,0 +1,9 @@
+<?php
+return (object) array(
+    'server' => 'dc1.domain.com',
+    'username' => 'cn=myadmin,ou=staff,dc=domain,dc=com',
+    'password' => 'MySecurePassword',
+    'searchBase' => 'ou=staff,dc=domain,dc=com',
+    'secretKey' => 'ASuperSecretAPIKey'
+);
+?>
