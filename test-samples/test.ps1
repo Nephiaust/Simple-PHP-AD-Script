eb0e6ae4dbf8bb0bcd7af3e9811775a7c9ec370e
Nephiaust <nephi.aust@gmail.com>
2022-08-16 17:24:19 +0930

    Update test.ps1
    Updated script to match new authorisation key location in JSON instead of URL

--- test-samples/test.ps1
+++ test-samples/test.ps1
@@ -1,10 +1,9 @@
 $body = @{
     username ="testuser"
     password = 'ASamplePassword'
+    AuthKey = 'ASuperSecretAPIKey'
 }
 
-$Key = "ASuperSecretAPIKey"
-
-$url = "https://dc1.domain.com/admgmt/password/$key"
+$url = "https://dc1.domain.com/admgmt/password/"
 
 Invoke-RestMethod -Method 'Post' -Uri $url -Body ($body|ConvertTo-Json) -ContentType "application/json"
